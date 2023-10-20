09b983c8aa177330da52f6f8ead875f9167e9181
akashes <139908260+akashes@users.noreply.github.com>
2023-10-20 18:41:28 +0530

    Add files via upload

---
+++ canvas/main.js
@@ -0,0 +1,82 @@
+// for(var i=1;i<=5;i++){
+//     space = " ";
+
+//     string=" "
+//     for(var k=1;k<(i-2);k++){
+//         space+="/"
+//     }
+//     console.log(space);
+//     for(var j=1;j<=i;j++){
+//         string+=" *"
+//     }
+//     console.log(string);
+
+ 
+// }
+
+// for(var i=4;i>=1;i--){
+//     space=""
+//     string=""
+//     for(var k=1;k<i;k++){
+//         space+=" "
+//     }
+//     for(var j=4;j>=i;j--){
+//         string+="* "
+//     }
+//     result=space+string
+//     console.log(result);
+
+// }
+
+// row=10
+// for(var i=1;i<=row;i++){
+//     space=" "
+//     string=" "
+//     for(var k=row;k>i;k--){
+//         space+=" "
+//     }
+//     for(var j=1;j<=i;j++){
+//         string+="* "
+
+//     }
+  
+//     result=space+string
+//     console.log(result);}
+
+
+    // for(var i=1;i<=4;i++){
+    //     string=""
+    //     for(var j=1;j<=7;j++){
+    //         if(i==4){
+    //             string+="*"
+    //         }else if(i+j==5 || j-i==3 ){
+    //             string+="*"                //nice one
+    //         }
+    //         else(
+    //             string+=" "
+    //         )
+          
+    //     }
+    //     console.log(string);
+    // }
+
+
+
+
+
+    // var global="global"
+
+
+
+    // function parent(){
+    //     parent="parent"
+    //     console.log(parent,global);  //nested functions , closure concepts
+
+    //     return child=()=>{
+    //         child="child"
+    //         console.log(child,parent,global);
+    //     }
+    // }
+    //  disp=parent()
+    // disp()
+
